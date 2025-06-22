a755041a9fa529b686f35aa186703cc839193fb1
David Susanto <david.susanto@stockbit.com>
2025-06-22 21:50:17 +0700

    update build.gradle update BasePage.java update LoginPage.java

---
+++ src/test/java/com/saucedemo/pages/LoginPage.java
@@ -0,0 +1,9 @@
+package com.saucedemo.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends BasePage {
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+}
